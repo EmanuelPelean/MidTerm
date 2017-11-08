9cebfc125f963dc86a973356e96ff1857334945c
NinjawarriorMP <32735104+NinjawarriorMP@users.noreply.github.com>
2017-11-08 11:36:07 -0500

    initial commit

---
+++ src/Board.java
@@ -0,0 +1,12 @@
+
+public class Board {
+
+	public Board() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
