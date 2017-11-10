74b9d4660434195d93e38c09404df562eff04c02
Serhiy Bardysh <sergiooua@gmail.com>
2017-11-10 14:13:38 -0500

    additional refactoring to LibraryApp

--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -10,15 +10,19 @@ public class LibraryApp {
 
 		ArrayList<Books> booksArray = lib1.translateDoc();
 
-		System.out.println("Welcome to the Grand Circus Library Terminal\n");
-
+		//System.out.println("Welcome to the Grand Circus Library Terminal\n");
+		System.out.println("   ______                     __   _______                         __    _ __                             \r\n" + 
+				"  / ____/________ _____  ____/ /  / ____(_)___________  _______   / /   (_) /_  _________ ________  __    \r\n" + 
+				" / / __/ ___/ __ `/ __ \\/ __  /  / /   / / ___/ ___/ / / / ___/  / /   / / __ \\/ ___/ __ `/ ___/ / / /    \r\n" + 
+				"/ /_/ / /  / /_/ / / / / /_/ /  / /___/ / /  / /__/ /_/ (__  )  / /___/ / /_/ / /  / /_/ / /  / /_/ /     \r\n" + 
+				"\\____/_/   \\__,_/_/ /_/\\__,_/   \\____/_/_/   \\___/\\__,_/____/  /_____/_/_.___/_/   \\__,_/_/   \\__, /      \r\n" + 
+				"                                                                                             /____/       ");
 		while (proceed) {
-			System.out.println("\n");
 			int userSelection = Validator.getInt(scan,
-					"Please select from the following options (enter an number):\n "
-							+ "\n1. Display the entire list of books.\n " + "\n2. Search for a book by author: \n"
-							+ "\n3. Search for a book by title.\n " + "\n4. Return a book: \n"
-							+ "\n5. Add a book to database: \n " + "\n6. Exit \n\n",1, 6);
+					"\nPlease select from the following options (enter an number):\n "
+							+ "\n1. Display the entire list of books: " + "\n2. Search for a book by author: "
+							+ "\n3. Search for a book by title: " + "\n4. Return a book: "
+							+ "\n5. Add a book to database:  " + "\n6. Exit. \n\n",1, 6);
 
 			switch (userSelection) {
 			case 1:
